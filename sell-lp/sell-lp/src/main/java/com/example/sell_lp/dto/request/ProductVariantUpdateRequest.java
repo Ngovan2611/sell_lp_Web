886c1cd1fd84b9e2b09e6f2df2000acfcb30b0a1
ADMIN <ngodang729@gmail.com>
2026-05-18 19:07:54 +0700

    product-change-config commit

--- sell-lp/sell-lp/src/main/java/com/example/sell_lp/dto/request/ProductVariantUpdateRequest.java
+++ sell-lp/sell-lp/src/main/java/com/example/sell_lp/dto/request/ProductVariantUpdateRequest.java
@@ -1,5 +1,7 @@
 package com.example.sell_lp.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,7 +15,11 @@ import lombok.experimental.FieldDefaults;
 @Data
 public class ProductVariantUpdateRequest {
     private Long variantId;
+    @NotNull(message = "Giá bán không được để trống")
+    @Min(value = 0, message = "Giá bán phải lớn hơn hoặc bằng 0")
     private Double price;
+    @NotNull(message = "Số lượng kho không được để trống")
+    @Min(value = 0, message = "Số lượng kho phải lớn hơn hoặc bằng 0")
     private Integer stockQty;
 
 }
